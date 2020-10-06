22681e1a4b67056f88408083c29d0cdb74520983
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2020-10-06 18:02:33 +0100

    job no centro de emprego de carregar caixas

---
+++ widlakjob/server/main.lua
@@ -0,0 +1,86 @@
+---======================---
+---Written by Tościk#9715---
+---======================---
+
+
+local hangar1ID = nil
+local hangar2ID = nil
+local gotowka = {a = 15, b = 35} -- zakres od ile do ile wynosi gotowka za wykonanie misji
+-----------------------------------
+local MisjaAktywna = 0
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj)
+	ESX = obj
+end)
+
+AddEventHandler('playerDropped', function(DropReason)
+
+	if hangar1ID == source then
+	hangar1ID = nil
+	end
+	
+	if hangar2ID == source then
+	hangar2ID = nil
+	end
+	
+end)
+
+RegisterServerEvent('tostdostawa:przejmijHangar')
+AddEventHandler('tostdostawa:przejmijHangar', function(ktory)
+
+if ktory == '1' then
+	if hangar1ID == nil then
+		hangar1ID = source
+		TriggerClientEvent('esx:showNotification', source, '~g~You are taking over the hangar.')
+		TriggerClientEvent('esx:showNotification', source, '~g~Now only you can perform orders here.')
+		TriggerClientEvent("tostdostawa:maszHangar", source)
+	else
+		TriggerClientEvent('esx:showNotification', source, '~y~This hangar is already taken by ID ~r~'..hangar1ID)
+	end
+elseif ktory == '2' then
+	if hangar2ID == nil then
+		hangar2ID = source
+		TriggerClientEvent('esx:showNotification', source, '~g~You arere taking over the hangar.')
+		TriggerClientEvent('esx:showNotification', source, '~g~Now only you can perform orders here.')
+		TriggerClientEvent("tostdostawa:maszHangar", source)
+	else
+		TriggerClientEvent('esx:showNotification', source, '~y~This hangar is already taken by ID ~r~'..hangar2ID)
+	end
+end
+
+end)
+
+
+
+RegisterServerEvent('tostdostawa:wykonanieMisji')
+AddEventHandler('tostdostawa:wykonanieMisji', function(premia)
+local _source = source
+local xPlayer = ESX.GetPlayerFromId(_source)
+local LosujSiano = math.random(gotowka.a,gotowka.b)
+
+if premia == 'nie' then
+xPlayer.addMoney(LosujSiano)
+TriggerClientEvent('esx:showNotification', _source, '~g~You get '..LosujSiano..'$ for delivery')
+Wait(2500)
+else
+xPlayer.addMoney(premia)
+Wait(2500)
+end
+
+end)
+
+RegisterServerEvent('tostdostawa:OdszedlDalekoo')
+AddEventHandler('tostdostawa:OdszedlDalekoo', function()
+
+	if hangar1ID == source then
+	hangar1ID = nil
+	end
+	
+	if hangar2ID == source then
+	hangar2ID = nil
+	end
+	
+end)
+
+
---
+++ widlakjob/sql.sql
@@ -0,0 +1,5 @@
+INSERT INTO `jobs` (name, label,whitelisted) VALUES
+('widlak', 'widlak', 0);
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+('widlak', 0, 'employee', 'Employee', 750, '{}','{}');
